40de310881e09720d2382da463001659acfe2df3
Alexander Afanasiev <armawir1@mail.ru>
2023-01-30 21:23:25 +0700

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -15,6 +15,11 @@ mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
 app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://mestoalex.students.nomoredomains.rocks');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
